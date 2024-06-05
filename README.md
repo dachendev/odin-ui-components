12addab2a6ebfdb410c2ca451e72ea9678660929
Jacob Dachenhaus <dachendev@gmail.com>
2024-06-05 11:25:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## How do I publish to GitHub Pages?
+
+Once you're ready to publish, do the following:
+
+1. Remove dist from your gitignore file
+2. Run `npm run build`
+3. Commit & push the latest build
+4. Run `npm run publish`
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/main.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  // Uncomment this if you have multiple entry points
+  // optimization: {
+  //     runtimeChunk: 'single',
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
